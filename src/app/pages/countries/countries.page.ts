9b021d128c9c155640303980b11c1d8cc91ee5e4
cisse410 <dramisse90@gmail.com>
2025-01-02 12:34:11 +0000

    ajout de la barre de navigation

---
+++ src/app/pages/countries/countries.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-countries',
+  standalone: true,
+  imports: [],
+  templateUrl: './countries.page.html',
+  styleUrl: './countries.page.css'
+})
+export class CountriesPage {
+
+}
