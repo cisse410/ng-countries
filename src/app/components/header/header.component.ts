5b31b2c8dd273d2bf76a72f30752dd6eb2f9a083
cisse410 <dramisse90@gmail.com>
2025-01-02 20:18:27 +0000

    Ajout de l'input pour la recherche d'un pays

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { SearchComponent } from '../search/search.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, SearchComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
